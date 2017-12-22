d7e34822706eefd83616ebfa3f1ffe33e1af619e
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2017-12-21 16:49:23 -0800

    allow passing options

--- index.js
+++ index.js
@@ -4,6 +4,11 @@
 module.exports = {
   name: 'ember-cli-tslint',
 
+  _getTSLintOptions() {
+  	const options = (this.parent && this.parent.options) || (this.app && this.app.options) || {};
+  	return options['ember-cli-tslint'] || {};
+  },
+
   lintTree(type, tree) {
     if (type === 'templates') {
       return undefined;
@@ -12,7 +17,6 @@ module.exports = {
     const TSLint = require('broccoli-tslinter');
 
 
-    return new TSLint(tree, {
-    });
+    return new TSLint(tree, _getTSLintOptions());
   }
 };
